1059ddef2f3b6c86b5dab160286744b957288f05
Diego Chavão <diegochavao+git@proton.me>
2026-04-02 12:32:33 -0300

    Remove obsolete vm runtime modules after executor migration

--- src/runtime/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod host;
-pub mod io;
-pub mod object;
-pub mod scope;
-pub mod value;
-pub mod vm;
-
-pub use io::{OutputEvent, OutputMode, RuntimeIo, StdIo, TestIo};
-pub use vm::{Vm, VmError};
